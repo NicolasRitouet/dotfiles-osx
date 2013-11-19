9b341db02752980ce49f34aadb49c315cf213003
Nicolas Ritouet <nico2706@gmail.com>
2013-11-19 11:01:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
 function doIt() {
-        rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+        # http://mpov.timmorgan.org/use-rsync-instead-of-cp/
+        rsync --exclude ".git/" --exclude ".DS_Store" --exclude "install.sh" \
                 --exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
         source ~/.bash_profile
 }
